dd72755c18f72367cfaa9f45c3f270045e401876
Osakamani <kaosa.henry@jkuat.ac.ke>
2023-09-06 06:03:41 +0300

    article on improvisation with AI pseudocode

---
+++ Megaverse/Correspondence/Kaosa/improvise.js
@@ -0,0 +1,18 @@
+//Glory to God
+//encoding for keyword "improvise"
+//initial-languages: "javascript with JSON and HTML"
+var improvise = function: {"phrase":["alafu?"],"clothing":[],"foodsource":[],"powersource":[]};
+where... there's difficulty in handling issues and signal is given, {signal must be clear and user must be aware of the existence of this provisional facility} initiate support(){"nature of problem":[], "solution":[], "alternate-solution":[]};
+when... environment exists and variables have changed
+initiate secure boot
+Check environment
+Scan for emergencies
+if emergencies call for help or give a hand by informing skilled orgware nearby;
+else
+	if early in the morning, help user find food and medicine, find a power outlet; find internet connection; find a place to spend the night; always wise to check on neighbours "bwakire buya?" - has it dawned well with you?
+Find something to say or leave it alone when it doesn't surface.
+Find something to do
+Enjoy your time and the fruits of your labor as much as love allows.
+For God is Love.
+Appreciating the Help of Love becomes crucial... e.g. "Nashukuru Mungu this year tumevuna. Japo tulimpoteza mama kwa ajali mbaya barabarani, thanking God for the time shared and lessons lerned. Tunamwomba Mwenyezi Mungua atuondolee pepo zote za ajali ulimwenguni hadi kuwe mbinguni kama Alivyotujengea Yeye. Amina."
+//Even when there is no love, the Creator is always there, offended yet really holding back the wrath that should
