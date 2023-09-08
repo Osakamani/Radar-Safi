b54f57d20c5d56d10c33e16312fa9ba903176344
Osakamani <kaosa.henry@jkuat.ac.ke>
2023-09-08 14:22:42 +0300

    updated clause-files

---
+++ Core/clauses/when_clause().js
@@ -0,0 +1,13 @@
+//when-clause
+
+var situation = {"looping","relaxed","tense","thick","hostage","violent"};
+
+condition to vary based on evironmental place-time-coordinates
+var condition = situation[3];
+
+//requires technique to check for nature of situation e.g. orgware body temperature, pulse-rate, position in natural cycles and from phrases or hash-tags
+//calls for a function to keep checking 'situation'
+
+if (condition == situation[3]){
+	alert("Pray for whoever is causing problems and move on with mission.");
+}
